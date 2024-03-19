cfe6a6bc1b392421ccbcfe2494c1935c76153eea
evelyncampos12 <eve.campos17@gmail.com>
2024-03-19 18:56:40 -0300

    feat: create user

---
+++ user.js
@@ -0,0 +1,17 @@
+const user = {
+  name: "evelyn",
+  city: "maringa",
+  work: "studying to be a develope",
+  hobby: "sports and play video games",
+};
+
+console.log(
+  "Name: ",
+  user.name,
+  "City: ",
+  user.city,
+  "Work: ",
+  user.work,
+  "Hobby: ",
+  user.hobby
+);
