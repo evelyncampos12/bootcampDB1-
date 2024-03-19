373901fe2655e182aff49efe7b601c20464d6523
evelyncampos12 <eve.campos17@gmail.com>
2024-03-19 10:00:01 -0300

    refactor(pares.js): improove variable name and console.log

--- pares.js
+++ pares.js
@@ -1,5 +1,5 @@
 const array = [2, 4, 3, 1, 6, 7, 9, 6, 10, 17];
 
-const pares = array.filter((item) => item % 2 === 0);
+const numerosPares = array.filter((item) => item % 2 === 0);
 
-console.log(pares);
+console.log("os numeros pares do array", numerosPares);
