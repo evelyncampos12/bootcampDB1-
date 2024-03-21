dc1ffd2d542c27ab4d35ee67c14c0d397201c9b8
evelyncampos12 <eve.campos17@gmail.com>
2024-03-21 20:53:59 -0300

    feat:editando arquivo impares

--- impares.js
+++ impares.js
@@ -1,5 +1,5 @@
-const array = [2, 4, 3, 1, 6, 7, 9];
+const array = [3, 1, 6, 7, 9, 78, 90, 25];
 
 const impares = array.filter((item) => item % 2 !== 0);
 
-console.log(impares);
+console.log("Esses são os numeros impares:", impares);
