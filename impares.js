92c2b5f47214cc26a073da25dc3fd86d505ec7ee
evelyncampos12 <144143140+evelyncampos12@users.noreply.github.com>
2024-03-21 20:50:53 -0300

    Update impares.js

--- impares.js
+++ impares.js
@@ -1,5 +1,5 @@
-const array = [2, 4, 3, 1, 6, 7, 9, 10, 13, 15];
+const array = [2, 4, 3, 1, 6, 7, 9, 10, 13, 15, 57, 89];
 
 const impares = array.filter((item) => item % 2 !== 0);
 
-console.log("Numero impares:", impares);
+console.log("Os Numero impares:", impares);
